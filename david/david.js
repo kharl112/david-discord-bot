902323314f65e82d60c1f271a9ae4fcc51d286f5
kharl112 <kharl_yumul@yahoo.com>
2020-12-15 15:11:17 -0800

    fixed mentioning from server.js

--- david/david.js
+++ david/david.js
@@ -7,19 +7,14 @@ module.exports = message => {
     let d = message.content.split("").slice(0, 2);
     d = d.join("").toString();
     let user = message.mentions.users.first() || {id: ""};
-
     if (d === 'd?') {
         message.content === 'd?' ? ping(message) : null;
 
         message.content === 'd? register' ? user_register(message) : null;
 
-        message.content === 'd? av' ||
-            message.content === `d? av <@!${user.id}>`
-            ? view_avatar(message) : null;
 
-        message.content === 'd? prof' ||
-            message.content === `d? prof <@!${user.id}>`
-            ? view_profile(message) : null;
+            /d\? av/gi.test(message.content) ? view_avatar(message) : null;
+            /d\? prof/gi.test(message.content) ? view_profile(message) : null;
     }
 
     let has_m_points = message.content.match(/metal|dave mustaine|metallica|megadeth|david ellefson|80s|david/gi);
