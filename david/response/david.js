d32ea589a972190b4a6bc5374addcce552d2aed1
kharl112 <kharl.dms112@gmail.com>
2021-03-04 21:58:36 -0600

    code refactored in david.js

--- david/response/david.js
+++ david/response/david.js
@@ -1,10 +1,12 @@
+const Misc = require("./misc/misc");
+
 module.exports = (msg) => {
   const david = msg.content.split("").slice(0, 2).join("");
   if (david.toLowerCase() === "d?") {
     /d\? av/i.test(msg.content)
-      ? msg.reply("avatar?")
+      ? Misc.view_av(msg)
       : /d\?/i.test(msg.content)
-      ? msg.reply("What?")
+      ? Misc.ping(msg)
       : null;
   }
 };
