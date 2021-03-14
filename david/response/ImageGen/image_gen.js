32705228cd146d1da36419227041d6218c4f2e37
kharl112 <kharl.dms112@gmail.com>
2021-03-14 01:30:16 -0600

    image gen for single word completed

---
+++ david/response/ImageGen/image_gen.js
@@ -0,0 +1,27 @@
+const singleWord = require("./templates/singleWord");
+
+module.exports = (() => {
+  const pass = (msg) =>
+    singleWord(
+      msg,
+      "https://indianmemetemplates.com/wp-content/uploads/Quiz-Kid.jpg",
+      "pass",
+      { x: 390, y: 480 }
+    );
+
+  const facts = (msg) =>
+    singleWord(
+      msg,
+      "https://64.media.tumblr.com/b622785d12fc8097bec915cca0dbef29/tumblr_p86etmYqXf1xorsigo1_1280.jpg",
+      "facts",
+      { x: 100, y: 650 }
+    );
+
+  const cmm = (msg) =>
+    singleWord(msg, "https://imgflip.com/s/meme/Change-My-Mind.jpg", "cmm", {
+      x: 230,
+      y: 225,
+    });
+
+  return { pass, facts, cmm };
+})();
--- david/response/david.js
+++ david/response/david.js
@@ -1,12 +1,18 @@
 const Misc = require("./Misc/misc");
 const Most = require("./Most/most");
-
+const ImageGen = require("./ImageGen/image_gen");
 module.exports = (msg) => {
   const david = msg.content.split("").slice(0, 2).join("");
   if (david.toLowerCase() === "d?") {
     /d\? av/i.test(msg.content)
-      ? Misc.view_av(msg) :
-      /d\? roast/i.test(msg.content)
+      ? Misc.view_av(msg)
+      : /d\? pass/i.test(msg.content)
+      ? ImageGen.pass(msg)
+      : /d\? facts/i.test(msg.content)
+      ? ImageGen.facts(msg)
+      : /d\? cmm/i.test(msg.content)
+      ? ImageGen.cmm(msg)
+      : /d\? roast/i.test(msg.content)
       ? Misc.roast(msg)
       : /d\? gay/i.test(msg.content)
       ? Most.gay(msg)
