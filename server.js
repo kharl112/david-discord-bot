8498b2d7c890970d93dbb8401dc8063f88bd7271
kharl112 <kharl.dms112@gmail.com>
2021-03-17 02:39:41 -0500

    presence has already been set

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-const Discord = require('discord.js');
-require('dotenv').config();
+const Discord = require("discord.js");
+require("dotenv").config();
 const client = new Discord.Client();
-const mongoose = require('mongoose');
-const david = require('./david/response/david');
+const mongoose = require("mongoose");
+const david = require("./david/response/david");
 
-client.on('ready', () => {
-    console.log(`Ayyyyy! ${client.user.tag} finallly woke up!`);
+client.on("ready", () => {
+  console.log(`Ayyyyy! ${client.user.tag} finallly woke up!`);
+  client.user.setPresence({
+    status: "dnd",
+    activity: {
+      name: "sea shanties and singing with my crew while we roam in the vast sea of west indies",
+      type: 2,
+      url: "https://open.spotify.com/track/4n5UwoPQn0tBlqxQjyEMdB?si=7079619f3f794fd7"
+    },
+  });
 });
 
-client.on('message', async message => {
-   david(message);
+client.on("message", async (message) => {
+  david(message);
 });
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology : true
-}, () => console.log('david is connected to the database'));
+    useUnifiedTopology: true,
+  },
+  () => console.log("david is connected to the database")
+);
 
-
-client.login(process.env.API_KEY)
+client.login(process.env.API_KEY);
