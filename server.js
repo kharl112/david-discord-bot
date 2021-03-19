7749641125dc65233256fd714d16f111f82234fa
kharl112 <kharl.dms112@gmail.com>
2021-03-18 23:23:28 -0500

    file refactorization completed in assets>js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const presence = require("./src/assets/js/misc/presence");
+const presence = require("./src/assets/js/client/status/presence");
 const david = require("./src/responses/david");
 require("dotenv").config();
 
--- src/responses/david.js
+++ src/responses/david.js
@@ -1,4 +1,4 @@
-const messageFlags = require("../assets/js/misc/messageContentFlags");
+const messageFlags = require("../assets/js/message/content/contentFlags");
 
 module.exports = (message) => {
   const [command, flag] = messageFlags(message);
