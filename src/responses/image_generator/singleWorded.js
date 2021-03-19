bebf3ca308d3a874d573effbf20052f436116a8a
kharl112 <kharl.dms112@gmail.com>
2021-03-19 02:09:32 -0500

    version changed

--- src/responses/image_generator/singleWorded.js
+++ src/responses/image_generator/singleWorded.js
@@ -2,17 +2,17 @@ const singles = require("./templates/singles");
 const getFileLoc = require("../../assets/js/image/getFileLoc");
 
 module.exports = (() => {
+  const present = ({ message, flag }) =>
+    singles(message, getFileLoc(flag), { x: 120, y: 85, fontSize: 128 });
+
   const attach = ({ message, flag }) =>
     singles(message, getFileLoc(flag), { x: 250, y: 40, fontSize: 64 });
-
-  const burn = ({ message, flag }) =>
-    singles(message, getFileLoc(flag), { x: 20, y: 35, fontSize: 32 });
 
   const jerry = ({ message, flag }) =>
     singles(message, getFileLoc(flag), { x: 220, y: 420, fontSize: 64 });
 
-  const present = ({ message, flag }) =>
-    singles(message, getFileLoc(flag), { x: 120, y: 85, fontSize: 128 });
+  const burn = ({ message, flag }) =>
+    singles(message, getFileLoc(flag), { x: 20, y: 35, fontSize: 32 });
 
   const petition = ({ message, flag }) =>
     singles(message, getFileLoc(flag), { x: 70, y: 125, fontSize: 32 });
