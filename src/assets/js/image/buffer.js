e84fe225723c36e035e839d48cc65aa58ce8fe66
kharl112 <kharl.dms112@gmail.com>
2021-03-18 23:33:54 -0500

    buffer function fixed

--- src/assets/js/image/buffer.js
+++ src/assets/js/image/buffer.js
@@ -2,5 +2,5 @@ const { MessageAttachment } = require("discord.js");
 
 module.exports = (img64, user) => {
   const imageBuff = new Buffer.from(img64.split(",")[1], "base64");
-  return new MessageAttachment(imageBuff, `${user.nickname}.png`);
+  return new MessageAttachment(imageBuff, `${user.username}.png`);
 };
