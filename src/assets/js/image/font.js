98f76521e83cff228ff188254a09c8b24ed798a1
kharl112 <kharl.dms112@gmail.com>
2021-03-18 23:35:43 -0500

    font function fixed

--- src/assets/js/image/font.js
+++ src/assets/js/image/font.js
@@ -1,6 +1,6 @@
 const Jimp = require("jimp");
 
 module.exports = (fontSize, color = "BLACK") => {
-    color = color.toUpperCase();
-    return Jimp[`FONT_SANS_${fontSize}_${color}`];
+  color = color.toUpperCase();
+  return Jimp.loadFont(Jimp[`FONT_SANS_${fontSize}_${color}`]);
 };
