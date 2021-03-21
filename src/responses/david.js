fcc43cec8003b35d69e8ccab73f581f32669f928
kharl112 <kharl.dms112@gmail.com>
2021-03-20 23:38:02 -0500

    code refactored in david.js

--- src/responses/david.js
+++ src/responses/david.js
@@ -5,31 +5,10 @@ const avatarPlacement = require("./image_generator/avatarPlacement");
 module.exports = (message) => {
   const [command, flag] = messageFlags(message);
 
-  if (command.toLowerCase() === "d?") {
-    /attach/i.test(flag)
-      ? singleWorded.attach({ message, flag })
-      : /burn/i.test(flag)
-      ? singleWorded.burn({ message, flag })
-      : /jerry/i.test(flag)
-      ? singleWorded.jerry({ message, flag })
-      : /present/i.test(flag)
-      ? singleWorded.present({ message, flag })
-      : /realize/i.test(flag)
-      ? singleWorded.realize({ message, flag })
-      : /petition/i.test(flag)
-      ? singleWorded.petition({ message, flag })
-      : /whisper/i.test(flag)
-      ? singleWorded.whisper({ message, flag })
-      : /drip/i.test(flag)
-      ? avatarPlacement.drip({ message, flag })
-      : /throw/i.test(flag)
-      ? avatarPlacement.brainThrow({ message, flag })
-      : /surrounded/i.test(flag)
-      ? avatarPlacement.surrounded({ message, flag })
-      : /bother/i.test(flag)
-      ? avatarPlacement.bother({ message, flag })
-      : /cum/i.test(flag)
-      ? avatarPlacement.cum({ message, flag })
-      : null;
+  if (command.toLowerCase() === "d?" && flag) {
+    const parent = [singleWorded, avatarPlacement].filter((node) =>
+      node.hasOwnProperty(flag)
+    );
+    if (parent[0]) return parent[0][flag]({message, flag});
   }
 };
