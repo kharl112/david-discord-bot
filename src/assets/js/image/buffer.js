7040da154de3388a2b79664dd214f93f3e78bc37
kharl112 <kharl.dms112@gmail.com>
2021-03-21 02:24:13 -0500

    image buffer added new parameter for file extensions of images

--- src/assets/js/image/buffer.js
+++ src/assets/js/image/buffer.js
@@ -1,6 +1,6 @@
 const { MessageAttachment } = require("discord.js");
 
-module.exports = (img64, user) => {
+module.exports = (img64, user, ext = "png") => {
   const imageBuff = new Buffer.from(img64.split(",")[1], "base64");
-  return new MessageAttachment(imageBuff, `${user.username}.png`);
+  return new MessageAttachment(imageBuff, `${user.username}.${ext}`);
 };
