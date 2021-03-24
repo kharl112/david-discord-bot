29d80c9e104d02eed2ab88a6fabdf22dfcc7b37b
kharl112 <kharl.dms112@gmail.com>
2021-03-24 00:31:45 -0500

    contentFlags modified

--- src/assets/js/message/content/contentFlags.js
+++ src/assets/js/message/content/contentFlags.js
@@ -1,10 +1,14 @@
 module.exports = (message) => {
   const content = message.content.split(" ");
+  const text = message.mentions.users.first()
+    ? content.slice(3).join(" ")
+    : content.slice(2).join(" ");
+
   if (content.length > 1)
     return ([command, flag, word] = [
       content[0],
       content[1].toLowerCase(),
-      content.slice(2).join(" "),
+      text,
     ]);
   else return ([command] = [content[0]]);
 };
