62ffa8fc0349153c9b38668086bd83d905bf8c95
kharl112 <kharl.dms112@gmail.com>
2021-03-25 02:50:36 -0500

    url matching for images fixed in imageAttachment file in assets

--- src/assets/js/message/content/imageAttachment.js
+++ src/assets/js/message/content/imageAttachment.js
@@ -4,9 +4,11 @@ module.exports = (message) => {
   let [, , word] = messageFlags(message);
   const { avatar } = userAvatar(message);
   const checkExt = (url) => /png$|jpg$|jpeg$/i.test(url);
-  return message.attachments
-    .map((node) => node.url)
-    .filter((url) => checkExt(url))[0] || checkExt(word)
-    ? word
-    : false || avatar;
+  return (
+    message.attachments
+      .map((node) => node.url)
+      .filter((url) => checkExt(url))[0] ||
+    (checkExt(word) ? word : false) ||
+    avatar
+  );
 };
