fbccdadd80e6ec1dc845b7472bab9dd087cae00c
kharl112 <kharl.dms112@gmail.com>
2021-03-25 00:16:54 -0500

    image_effects imported in david.js

--- src/responses/david.js
+++ src/responses/david.js
@@ -1,12 +1,12 @@
 const messageFlags = require("../assets/js/message/content/contentFlags");
 const singleWorded = require("./image_generator/single_worded/singleWorded");
 const avatarPlacement = require("./image_generator/avatar_placement/avatarPlacement");
+const imageEffects = require("./image_generator/image_effects/imageEffects");
 
 module.exports = (message) => {
   const [command, flag] = messageFlags(message);
-
   if (command.toLowerCase() === "d?" && flag) {
-    const parent = [singleWorded, avatarPlacement].filter((node) =>
+    const parent = [singleWorded, avatarPlacement, imageEffects].filter((node) =>
       node.hasOwnProperty(flag)
     );
     if (parent[0]) return parent[0][flag](message, flag);
