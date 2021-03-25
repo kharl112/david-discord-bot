6ede1be16a332a2f90cb173bff0e66c82e78eed0
kharl112 <kharl.dms112@gmail.com>
2021-03-25 00:16:02 -0500

    imageAttachment file in assets created"

---
+++ src/assets/js/message/content/imageAttachment.js
@@ -0,0 +1,12 @@
+const messageFlags = require("./contentFlags");
+const userAvatar = require("../user/userAvatar");
+module.exports = (message) => {
+  let [, , word] = messageFlags(message);
+  const { avatar } = userAvatar(message);
+  const checkExt = (url) => /png$|jpg$|jpeg$/i.test(url);
+  return message.attachments
+    .map((node) => node.url)
+    .filter((url) => checkExt(url))[0] || checkExt(word)
+    ? word
+    : false || avatar;
+};
