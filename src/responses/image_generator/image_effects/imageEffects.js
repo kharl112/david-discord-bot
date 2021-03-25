33529376f0d47f94f3b3f3847eac2a52eff88e59
kharl112 <kharl.dms112@gmail.com>
2021-03-25 03:00:34 -0500

    sepia command completed

--- src/responses/image_generator/image_effects/imageEffects.js
+++ src/responses/image_generator/image_effects/imageEffects.js
@@ -3,5 +3,6 @@ const imageEffects = require("./templates/default");
 module.exports = (() => {
   const invert = (message, flag) => imageEffects(message, flag);
   const greyscale = (message, flag) => imageEffects(message, flag);
-  return { invert, greyscale };
+  const sepia = (message, flag) => imageEffects(message, flag);
+  return { invert, greyscale, sepia };
 })();
