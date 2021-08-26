79d499caf9dd9c2538f9482f4655091f7e2f2895
kharl <kharl.dms112@gmail.com>
2021-08-26 01:43:30 -0500

    embed created in imageSearch

--- src/responses/image_search/imageSearch.js
+++ src/responses/image_search/imageSearch.js
@@ -1,4 +1,29 @@
+const GoogleImages = require("google-images");
+const { MessageEmbed } = require("discord.js");
+require("dotenv").config();
+const client = new GoogleImages(process.env.CSE_ID, process.env.GOOGLE_API_KEY);
+const messageFlags = require("../../assets/js/message/content/contentFlags");
 module.exports = (() => {
-  const search = () => {};
+  const search = async (message, flag) => {
+    try {
+      const [, , word] = messageFlags(message);
+      const images = await client.search(word);
+      const filteredImages = images.filter((img) => img.url.endsWith("jpg"));
+      console.log(filteredImages);
+      const embed = new MessageEmbed()
+        .setImage(filteredImages[0].url)
+        .setDescription(filteredImages[0].description)
+        .setAuthor(message.author.username, message.author.displayAvatarURL())
+        .setFooter(
+          `Google Images: 1 out of ${filteredImages.length} results`,
+          "https://wplawpractice.com/wp-content/uploads/2017/07/google-logo.jpg"
+        );
+
+      await message.react("👍");
+      return message.reply(embed);
+    } catch (error) {
+      return console.log(error);
+    }
+  };
   return { search };
 })();
