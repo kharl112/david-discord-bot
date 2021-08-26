9ead319edc235b1ed0eb730289fee73ab29ea10f
kharl <kharl.dms112@gmail.com>
2021-08-26 00:48:44 -0500

    imageSearch applied in david.js

--- src/responses/david.js
+++ src/responses/david.js
@@ -2,13 +2,17 @@ const messageFlags = require("../assets/js/message/content/contentFlags");
 const singleWorded = require("./image_generator/single_worded/singleWorded");
 const avatarPlacement = require("./image_generator/avatar_placement/avatarPlacement");
 const imageEffects = require("./image_generator/image_effects/imageEffects");
+const imageSearch = require("./image_search/imageSearch");
 
 module.exports = (message) => {
   const [command, flag] = messageFlags(message);
   if (command.toLowerCase() === "d?" && flag) {
-    const parent = [singleWorded, avatarPlacement, imageEffects].filter((node) =>
-      node.hasOwnProperty(flag)
-    );
+    const parent = [
+      singleWorded,
+      avatarPlacement,
+      imageEffects,
+      imageSearch,
+    ].filter((node) => node.hasOwnProperty(flag));
     if (parent[0]) return parent[0][flag](message, flag);
   }
 };
