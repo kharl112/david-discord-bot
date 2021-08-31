72a9f093dd7055ee0a3ab9227adca9edb5f1621c
kharl <kharl.dms112@gmail.com>
2021-08-31 07:09:56 -0500

    fixed contentFlags

--- src/assets/js/message/content/contentFlags.js
+++ src/assets/js/message/content/contentFlags.js
@@ -1,14 +1,14 @@
 module.exports = (message) => {
-  const content = message.content.split(" ");
-  const text = message.mentions.users.first()
-    ? content.slice(3).join(" ")
-    : content.slice(2).join(" ");
+	const content = message.content.split(" ");
+	const text = message.mentions.users.first()
+		? content.slice(2, content.length).join(" ")
+		: content.slice(2).join(" ");
 
-  if (content.length > 1)
-    return ([command, flag, word] = [
-      content[0],
-      content[1].toLowerCase(),
-      text,
-    ]);
-  else return ([command] = [content[0]]);
+	if (content.length > 1)
+		return ([command, flag, word] = [
+			content[0],
+			content[1].toLowerCase(),
+			text,
+		]);
+	else return ([command] = [content[0]]);
 };
