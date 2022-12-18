21c41185282039389c12bcaf1dd70c5930b49e0a
kharl112 <kharl.dms112@gmail.com>
2022-12-18 04:23:09 +0000

    added additional restriction on sending commands

--- src/responses/david.js
+++ src/responses/david.js
@@ -4,11 +4,15 @@ const avatarPlacement = require("./image_generator/avatarPlacement");
 
 module.exports = (message) => {
   const [command, flag] = messageFlags(message);
-
   if (command.toLowerCase() === "d?" && flag) {
-    const parent = [singleWorded, avatarPlacement].filter((node) =>
-      node.hasOwnProperty(flag)
-    );
-    if (parent[0]) return parent[0][flag]({message, flag});
+    //get existing props from parents
+    const parent = [singleWorded, avatarPlacement].filter((node) => node.hasOwnProperty(flag));
+
+    //return a message if parents is empty
+    if (!parent.length)
+      return message.channel.send(`Command does not exist`);
+
+    //return props as functions
+    if (parent[0]) return parent[0][flag]({ message, flag });
   }
 };
